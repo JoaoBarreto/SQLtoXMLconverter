129cdfbbfe32dde45542fb4033a3fe614152c603
JoaoBarreto <joaobarreto.ige@gmail.com>
2013-08-05 14:35:30 +0100

    ThreadPool Core
    Working Threadpool system,

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SQLtoXMLconverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SQLtoXMLConverter needs Folder to save files, number of working threads.
+            SQLtoXMLConverter converter = new SQLtoXMLConverter("C:/", 10);
+            converter.run();
+        }
+    }
+}
